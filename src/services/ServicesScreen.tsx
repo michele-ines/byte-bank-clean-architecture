d7f14c56c150b5fb36a3124500ed88d612435223
Michele Ines <michele.ine.ext@accesstage.com.br>
2025-09-07 13:16:11 -0300

    reafact: add newTransactio e balance em outros serviços.

--- src/services/ServicesScreen.tsx
+++ src/services/ServicesScreen.tsx
@@ -1,8 +1,12 @@
 import FinancialChart from "@/src/components/FinancialChart/FinancialChart";
 import { useWidgetPreferences } from "@/src/contexts/WidgetPreferencesContext";
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
+import Balance from "@/src/shared/cards/balance/BalanceComponent";
+import { CardListExtract } from "@/src/shared/cards/CardListExtract/CardListExtract"; // 👈 importa aqui
+import { UserInfo } from "firebase/auth";
+import { NewTransactionForm } from "../components/forms/NewTransactionForm/NewTransactionForm";
 import SavingsGoalWidget from "../shared/components/SavingsGoalWidget/SavingsGoalWidget";
 import SpendingAlertWidget from "../shared/components/SpendingAlertWidget/SpendingAlertWidget";
 import WidgetPreferencesButton from "../shared/components/WidgetPreferencesButton/WidgetPreferencesButton";
@@ -19,6 +23,12 @@ const transactions: Transaction[] = [
   { tipo: "saida", valor: 300 },
 ];
 
+const user = {
+  displayName: "Joana",
+} as UserInfo;
+
+const balance = { account: "corrente", value: 209021 };
+
 const ServicesScreen: React.FC = () => {
   const { preferences } = useWidgetPreferences();
 
@@ -28,7 +38,20 @@ const ServicesScreen: React.FC = () => {
         Outros serviços
       </Text>
 
+      {/* Botão de preferências */}
       <WidgetPreferencesButton />
+
+      {/* Balance */}
+      <View style={{ marginVertical: 16 }}>
+        <Balance balance={balance} user={user} />
+        <NewTransactionForm />
+        
+      </View>
+
+      {/* CardListExtract */}
+      <View style={{ marginBottom: 16 }}>
+        <CardListExtract title="Extrato" />
+      </View>
 
       <FinancialChart />
 
