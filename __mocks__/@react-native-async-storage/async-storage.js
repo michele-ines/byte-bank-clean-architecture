316c156a433a022dd82b127ce6cfd96eed82f761
Angélica Silva Leite <angelica.leite@cebraspe.org.br>
2025-12-01 15:34:01 -0300

    fix: ajustes de teste async storage

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,10 @@
-export * from "@react-native-async-storage/async-storage/jest/async-storage-mock";
+export default {
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  getAllKeys: jest.fn(),
+  multiGet: jest.fn(),
+  multiSet: jest.fn(),
+  multiRemove: jest.fn(),
+};
