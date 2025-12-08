1a7d01026c1191d87a9c52cd45e12fad5fa59114
Angélica Silva Leite <angelica.leite@cebraspe.org.br>
2025-12-08 16:08:33 -0300

    feat: config react-query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,30 @@
+import { queryClient } from "@/infrastructure/config/react-query-client";
 import { toastConfig } from "@/presentation/config/Toast.config";
 import { GlobalContextProvider } from "@presentation/state";
 import { LoggerProvider } from "@presentation/state/LoggerContext";
 import { WidgetPreferencesProvider } from "@presentation/state/WidgetPreferencesContext";
 import { layout } from "@presentation/theme";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
+import type { JSX } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
-
-import type { JSX } from "react";
 
 export default function RootLayout(): JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: layout.flex1 }}>
       <SafeAreaProvider>
-        <LoggerProvider>
-          <GlobalContextProvider>
-            <WidgetPreferencesProvider>
-              <Slot />
-              <Toast config={toastConfig} />
-            </WidgetPreferencesProvider>
-          </GlobalContextProvider>
-        </LoggerProvider>
+        <QueryClientProvider client={queryClient}>
+          <LoggerProvider>
+            <GlobalContextProvider>
+              <WidgetPreferencesProvider>
+                <Slot />
+                <Toast config={toastConfig} />
+              </WidgetPreferencesProvider>
+            </GlobalContextProvider>
+          </LoggerProvider>
+        </QueryClientProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
