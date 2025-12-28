1f0c223d4577bc74d29f75f98a300c6af5b7c03e
Larissa Storck <larissastorck@hotmail.com>
2025-12-28 16:19:25 -0300

    feat: removed visual log, logs show only in terminal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,9 +8,13 @@ import { layout } from "@presentation/theme";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import type { JSX } from "react";
+import { LogBox } from 'react-native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
+
+LogBox.ignoreAllLogs(true);
+
 
 export default function RootLayout(): JSX.Element {
   return (
